1e1adec9fa2adabed76b1f6ac51cc9fec4f82035
vijay466 <65277424+vijay466@users.noreply.github.com>
2024-04-26 11:34:33 +0530

    Update data.js

--- src/Data/data.js
+++ src/Data/data.js
@@ -8,15 +8,9 @@ export const data = [
     name: "NetflixGPT",
     image: netflix,
     github: "https://github.com/vijay466/NetflixGPT",
-    live: "https://netflixgpt-3.netlify.app/",
+    live: "https://netflix-gpt-3.netlify.app/",
   },
-  // {
-  //   id: 2,
-  //   name: "React JS Application",
-  //   image: RealEst,
-  //   github: "",
-  //   live: "",
-  // },
+
   {
     id: 3,
     name: "React JS Application",
